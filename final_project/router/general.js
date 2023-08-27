230d19fba50162d06c1c79d9069548add3a667db
3 <34marqam@gmail.com>
2023-08-27 12:17:14 -0400

    all tasks completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,6 +27,13 @@ public_users.get('/',function (req, res) {
   return res.send(JSON.stringify(book, null, 4));
 });
 
+// task 10
+public_users.get('/books', async (req, res) => {
+    await new Promise( resolve => {
+        resolve(res.send(JSON.stringify(books, null, 4)));
+    });
+});
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
@@ -35,6 +42,17 @@ public_users.get('/isbn/:isbn',function (req, res) {
     return res.send(books[isbn]);
   else
     return res.status(404).json({message: "Book not found!"});
+ });
+
+ // task 11
+ public_users.get('/async_isbn/:isbn', async (req, res) => {
+    const isbn = req.params.isbn;
+    await new Promise( (resolve) => {
+        if(isbn in books)
+            resolve(res.send(books[isbn]));
+        else
+            resolve(res.status(404).json({message: "Book not found!"}));
+    });
  });
   
 // Get book details based on author
@@ -49,6 +67,24 @@ public_users.get('/author/:author',function (req, res) {
     return res.send(book);
   return res.status(404).json({message: "Book not found!!"});
 });
+
+// task 12
+public_users.get('/async_author/:author',async (req, res) => {
+    //Write your code here
+    let book = {};
+    const author = req.params.author;
+
+    for(const key in books)
+      if(books[key]["author"] === author)
+          book[key] = books[key];
+    
+    await new Promise(resolve => {
+        if(Object.keys(book).length)
+            resolve(res.send(book));
+        else
+            resolve(res.status(404).json({message: "Book not found!!"}));
+    });
+  });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
@@ -66,6 +102,26 @@ public_users.get('/title/:title',function (req, res) {
     }
   return res.status(404).json({message: "Book not found!!"});
 });
+
+// task 13
+public_users.get('/async_title/:title',async (req, res) => {
+    const title = req.params.title;
+    let bookbytitle = {};
+    for(const key in books)
+        if(books[key]["title"] === title){
+            bookbytitle = {
+                'isbn': key,
+                'title': books[key]['title'],
+                'reviews': books[key]['reviews']
+            }
+        }
+    await new Promise(resolve =>{
+        if('isbn' in bookbytitle)
+            resolve(res.send(bookbytitle));
+        else
+            resolve(res.status(404).json({message: "Book not found!!"}));
+    });
+  });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
